422eb5389f374be062cfc0b1df464ba959101217
JacobMonksRev <jacob.monks@revature.com>
2022-06-10 17:09:32 -0400

    minor changes

--- Notes/Week3/Day5_Practice.py
+++ Notes/Week3/Day5_Practice.py
@@ -1,68 +1,70 @@
 """1. Write a Python function that creates a Counter given an inputed list.
       Print out the contents of the Counter in order from most instances to least.
 """
-# from collections import Counter
+from collections import Counter
 
-# def MyCounter(lst):
-#     c1 = Counter(lst)
-#     common = c1.most_common(len(lst))
+def MyCounter(lst):
+    c1 = Counter(lst)
+    common = c1.most_common(len(lst))
 
-#     for i in range(len(common)):
-#         print(common[i][0], ":", common[i][1])
+    for i in range(len(common)):
+        print(common[i][0], ":", common[i][1])
 
-# MyList = ["Jacob", "Rose", "Ryan", "Precious", "Rian", "Rose", "Jacob", "Jacob", "Ryan"]
-# MyCounter(MyList)
+MyList = ["Jacob", "Rose", "Ryan", "Precious", "Rian", "Rose", "Jacob", "Jacob", "Ryan"]
+MyCounter(MyList)
 
 """
 2. Write a Python function that returns a list of namedtuples given a list of tuples.
    Your namedtuples should also include an extra element at the end that incorporates a datetime object.
 """
-# from collections import namedtuple
-# import datetime
+from collections import namedtuple
+import datetime
 
-# def TupleNames(lst):
-#     OurNames = namedtuple("Students", ["name", "age", "dob", "time"])
-#     newlst = []
-#     for i in lst:
-#         newlst.append(OurNames(i[0], i[1], i[2], datetime.datetime.now().strftime("%X")))
+def TupleNames(lst):
+    OurNames = namedtuple("Students", ["name", "age", "dob", "time"])
+    newlst = []
+    for i in lst:
+        newlst.append(OurNames(i[0], i[1], i[2], datetime.datetime.now().strftime("%X")))
     
-#     return newlst
+    return newlst
 
-# MyList2 = [('John', 21, 2000), ('Ted', 34, 1988), ('Sarah', 12, 2010)]
-# newlist = TupleNames(MyList2)
-# for i in newlist:
-#     print(i)
+MyList2 = [('John', 21, 2000), ('Ted', 34, 1988), ('Sarah', 12, 2010)]
+newlist = TupleNames(MyList2)
+for i in newlist:
+    print(i)
+
 """
 3. Write a Python function that takes in a file and returns a list of emails
    that are found in that file. Be sure to use RegEx.
 """
-# import re      # RegEx - Regular Expressions
-# import os
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import re      # RegEx - Regular Expressions
+import os      # this is necessary to read and write to files.
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# def emails():
-#     content = open('emails.txt', 'r')
-#     finding = re.findall('\S+@\S+', content.read())
-#     print(finding)
-#     content.close()
+def emails():
+    content = open('emails.txt', 'r')
+    finding = re.findall('\S+@\S+', content.read())
+    print(finding)
+    content.close()
 
-# emails()
+emails()
 
-# Try for errors
+# Testing for errors and raising exceptions
 
-# import os
-# os.chdir(os.path.dirname(os.path.abspath(__file__)))
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-# x = input("Enter name, age, and year of birth separated by commas: ")
-# count = 0
-# for i in x:
-#     if i == ',':
-#         count += 1
-# if count > 2:
-#     raise Exception('You cannot have extra commas!')
-# myfile = open('csv_example.csv', 'a')
-# myfile.write("\n"+str(x))
-# myfile.close()
+x = input("Enter name, age, and year of birth separated by commas: ")
+count = 0
+for i in x:
+    if i == ',':
+        count += 1
+if count > 2:
+    raise Exception('You cannot have extra commas!')
+count = 0
+myfile = open('csv_example.csv', 'a')
+myfile.write("\n"+str(x))
+myfile.close()
 
 # try-except example
 
--- Notes/Week3/employee_example.py
+++ Notes/Week3/employee_example.py
@@ -1,5 +1,11 @@
 from lib2to3.pytree import Base
-import employee                                     # must import the file that contains the classes
+
+from psutil import users
+import employee                                    # must import the file that contains the classes
+import os
+import re
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 listEmp = []                                        # create empty lists to save our information
 listMan = []
@@ -8,9 +14,22 @@ listTrain = []
 def insert_emp():
     emp1 = employee.Employee                                # first indicate the file name, then the class name
     emp1.name = input("Enter the employee's first name: ")  # user input the information
-    emp1.salary = input("Enter the employee's salary: ")   
+    for i in emp1.name:                                     # checks each individual character in emp1.name
+        if not (i.isalpha() or i in ("'","-",".")) and i != ' ': # checks if the character is valid
+            raise Exception("Name contained an invalid character.") # raise exception if the character is not valid
+    while True:                                                     # loops indefinitely until broken
+        try:                                                        # try to execute this block of code
+            emp1.salary = int(input("Enter the employee's salary: "))
+        except ValueError:                                          # if the try-block found an exception, tell the user and try again
+            print(ValueError)
+            print("Salary was not valid")
+        else:                                                       # if there was NO exception, break the loop.
+            break
     emp1.dept = input("Enter the employee's dept: ")
-    myFile = open('newFile.txt', 'a')
+    for i in emp1.dept:                                     # checks each individual character in emp1.dept
+        if not i.isalpha():                                 # checks if the character is valid
+            raise Exception("Dept contained an invalid character.") # raise exception if the character is not valid
+    myFile = open('newFile.txt', 'a')                       # open our file to save the info
     myFile.write(str(emp1.type_employee) + ' ' + str(emp1.name) + ' ' + str(emp1.salary) + ' ' + str(emp1.dept) + '\n')
     myFile.close()
 
@@ -20,8 +39,21 @@ def insert_emp():
 def insert_man():
     man1 = employee.Manager
     man1.name = input("Enter the manager's first name: ")
-    man1.salary = input("Enter the manager's salary: ")
+    for i in man1.name:                                     # checks each individual character in emp1.name
+        if not (i.isalpha() or i in ("'","-",".")) and i != ' ': # checks if the character is valid
+            raise Exception("Name contained an invalid character.") # raise exception if the character is not valid
+    while True:                                                     # loops indefinitely until broken
+        try:                                                        # try to execute this block of code
+            man1.salary = int(input("Enter the employee's salary: "))
+        except ValueError:                                          # if the try-block found an exception, tell the user and try again
+            print(ValueError)
+            print("Salary was not valid")
+        else:                                                       # if there was NO exception, break the loop.
+            break
     man1.dept = input("Enter the manager's dept: ")
+    for i in man1.dept:                                     # checks each individual character in emp1.dept
+        if not i.isalpha():                                 # checks if the character is valid
+            raise Exception("Dept contained an invalid character.") # raise exception if the character is not valid
     myFile = open('newFile.txt', 'a')
     myFile.write(str(man1.type_employee) + ' ' + str(man1.name) + ' ' + str(man1.salary) + ' ' + str(man1.dept) + '\n')
     myFile.close()
@@ -32,8 +64,21 @@ def insert_man():
 def insert_trainee():
     train1 = employee.Trainee
     train1.name = input("Enter the trainee's first name: ")
-    train1.salary = input("Enter the trainee's salary: ")
+    for i in train1.name:                                     # checks each individual character in emp1.name
+        if not (i.isalpha() or i in ("'","-",".")) and i != ' ': # checks if the character is valid
+            raise Exception("Name contained an invalid character.") # raise exception if the character is not valid
+    while True:                                                     # loops indefinitely until broken
+        try:                                                        # try to execute this block of code
+            train1.salary = int(input("Enter the employee's salary: "))
+        except ValueError:                                          # if the try-block found an exception, tell the user and try again
+            print(ValueError)
+            print("Salary was not valid")
+        else:                                                       # if there was NO exception, break the loop.
+            break
     train1.dept = input("Enter the trainee's dept: ")
+    for i in train1.dept:                                     # checks each individual character in emp1.dept
+        if not i.isalpha():                                 # checks if the character is valid
+            raise Exception("Dept contained an invalid character.") # raise exception if the character is not valid
     myFile = open('newFile.txt', 'a')
     # Opens the file and then writes all the attibutes in the line below
     myFile.write(str(train1.type_employee) + ' ' + str(train1.name) + ' ' + str(train1.salary) + ' ' + str(train1.dept) + '\n')
@@ -50,7 +95,15 @@ def startup():                                  # the startup function is called
         print("\t3. Insert new trainee.")
         print("\t4. View all employees")
         print("\t5. Quit")
-        sel = int(input("\nSelection: "))       # take the user input for the selection
+        # We want to check for an error if the user inputs a non-integer value
+        while True:
+            try:
+                sel = int(input("\nSelection: "))       # take the user input for the selection
+            except ValueError:
+                print(ValueError)
+                print("Please Enter an Integer 1-5!")
+            else:
+                break
         if sel == 1:                            # if-else statement checks for each possible selection
             insert_emp()                        # runs the insert_emp() function defined above
         elif sel == 2:
