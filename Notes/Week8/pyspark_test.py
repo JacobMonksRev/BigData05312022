9b87bf7f342a8d36df0cc16af8dbcb9464fd6e2c
JacobMonksRev <jacob.monks@revature.com>
2022-07-11 12:35:23 -0400

    RDD Notes

--- Notes/Week8/pyspark_test.py
+++ Notes/Week8/pyspark_test.py
@@ -1,8 +1,12 @@
 from pyspark import SparkContext, sql, SparkConf
 from pyspark.sql import SparkSession, Row
+import pyspark
 
 # Create a SparkSession variable
 spark = SparkSession\
     .builder\
+    .master("local[1]")\
     .appName("Put appname here")\
     .getOrCreate()
+
+df1 = spark.createDataFrame()
