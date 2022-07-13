393fceeda23ca34b09d03fa6cef83164dc0f2f18
JacobMonksRev <jacob.monks@revature.com>
2022-07-13 12:55:17 -0400

    Wednesday Notes and Activity Folder restructure

--- Notes/Week8/pyspark_test.py
+++ Notes/Week8/pyspark_test.py
@@ -1,14 +1,41 @@
-from pyspark import SparkContext as sc, sql, SparkConf
-from pyspark.sql import SparkSession, Row
+from pyspark import SparkContext, sql, SparkConf
+from pyspark.sql import SparkSession, Row, HiveContext, SQLContext
 import pyspark
+from pyspark.sql.functions import col
+import os
+import sys
 
-# Create a SparkSession variable
+## Set up PySpark Environment Variables
+os.environ['PYSPARK_PYTHON'] = sys.executable
+os.environ['PYSPARK_DRIVER_PYTHON'] = sys.executable
+
+## Create a SparkSession variable
 spark = SparkSession\
     .builder\
-    .master("local[1]")\
-    .appName("Put appname here")\
+    .appName("App Name")\
+    .master("local[*]")\
     .getOrCreate()
 
-df1 = spark.createDataFrame()
+## .getOrCreate will either create a new SparkSession or search for the current one.
+## SparkContext is a part of SparkSession, so we can initialize SparkContext and call it 'sc':
+sc = spark.sparkContext
 
-rdd = sc.parallelize([1,2,3,4])
+## If we wish to initialize SQLContext:
+sqlcontext = SQLContext(spark)
+
+## We can use 'sc' to create RDDs and DataFrames
+rdd = sc.parallelize([(1,"Jacob","IT"),(2,"Bob","Sales"),(3,"Sam","IT"),(4,"Cindy","HR")])
+df = rdd.toDF(["ID","Name","Dept"])
+#df.show()
+#df2 = spark.read.option('header','true').csv('Notes/Week8/sample.csv')
+#df2.show()
+
+## And we can query the DataFrame
+#df.select("*").where(col("ID")=="1").show()
+
+## Query using Spark SQL
+# Spark SQL cannot query DataFrames directly
+#df2.createOrReplaceTempView("NamesDF")
+df.createOrReplaceTempView("Employees")
+# Creates a temporary view that gets saved in Hive Metastore
+spark.sql("SELECT Dept, count(*) FROM Employees GROUP BY Dept").show()
